7b7f56a719774b5e1552984e9b80722fcaa05753
afshin <shahinof10009@gmail.com>
2024-01-03 23:28:29 +0330

    set type to `module`

---
+++ dist/example/std-http/app.js
@@ -0,0 +1,14 @@
+import http from "node:http";
+const IP = "192.168.1.3";
+const PORT = 4090;
+const server = http.createServer((req, res) => {
+    const response = {
+        data: "accepted",
+    };
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.end(JSON.stringify(response));
+});
+server.listen(PORT, IP, () => {
+    console.log(`server conected at http:${IP}:${PORT}`);
+});
