8da10fd2c877f376fd060f531bb1d5edd935f9a3
afshin <shahinof10009@gmail.com>
2024-01-04 00:11:55 +0330

    some improvment

--- dist/example/std-http/app.js
+++ dist/example/std-http/app.js
@@ -1,4 +1,7 @@
 import http from "node:http";
+import os from "node:os";
+const ni = os.networkInterfaces();
+console.log(ni);
 const IP = "192.168.1.3";
 const PORT = 4090;
 const server = http.createServer((req, res) => {
@@ -10,5 +13,5 @@ const server = http.createServer((req, res) => {
     res.end(JSON.stringify(response));
 });
 server.listen(PORT, IP, () => {
-    console.log(`server conected at http:${IP}:${PORT}`);
+    console.log(`server conected at http://${IP}:${PORT}`);
 });
--- example/std-http/app.ts
+++ example/std-http/app.ts
@@ -1,6 +1,17 @@
 import http from "node:http";
+import os from "node:os";
 
-const IP = "192.168.1.3";
+// if you look for your local ip address
+// simply log ni constant to console
+// and look for connected network interface object in array
+const ni = os.networkInterfaces();
+
+// change IP to your local ip adrress
+// to access server from another machine
+// at the same local area network
+const IP = "localhost";
+
+// a random port
 const PORT = 4090;
 
 const server = http.createServer((req, res) => {
