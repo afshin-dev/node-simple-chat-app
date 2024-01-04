773354995d39dd4ca6bb7dffe9780636a8906a03
afshin <shahinof10009@gmail.com>
2024-01-04 23:46:31 +0330

    promisify version of clearLine

--- dist/utils.js
+++ dist/utils.js
@@ -4,3 +4,17 @@ export function moveCursor(dx, dy) {
         process.stdout.moveCursor(dx, dy, resolve);
     });
 }
+// clear current line
+// 1 : right of the cursor
+// -1 : left of the cursor
+// 0 : all the line
+export function clearLine(dir) {
+    return new Promise((resolve, reject) => {
+        process.stdout.clearLine(dir, resolve);
+    });
+}
+// clear 5 first number
+// console.log("0123456789");
+// await moveCursor(4, -1);
+// await clearLine(-1);
+// setTimeout(console.log, 3000);
--- utils.ts
+++ utils.ts
@@ -4,3 +4,22 @@ export function moveCursor(dx: number, dy: number) {
     process.stdout.moveCursor(dx, dy, resolve);
   });
 }
+
+type Direction = 1 | -1 | 0;
+
+// clear current line
+// 1 : right of the cursor
+// -1 : left of the cursor
+// 0 : all the line
+export function clearLine(dir: Direction) {
+  return new Promise<void>((resolve, reject) => {
+    process.stdout.clearLine(dir, resolve);
+  });
+}
+
+// clear 5 first number
+// console.log("0123456789");
+// await moveCursor(4, -1);
+// await clearLine(-1);
+
+// setTimeout(console.log, 3000);
